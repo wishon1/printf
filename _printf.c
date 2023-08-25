5c396dbb66343bbf5c63771648c430a1f089f1f0
wishon1 <wisdomhonest1@gmail.com>
2023-08-25 19:13:04 -0400

    printf

--- _printf.c
+++ _printf.c
@@ -16,7 +16,7 @@ int _printf(const char *format, ...)
 	count = 0;
 	i = 0;
 
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	if (format == NULL)
                 return (-1);
 
 	while (format[i] != '\0')
